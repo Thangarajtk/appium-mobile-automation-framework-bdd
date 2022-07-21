9d1095c0234390e17622b351ea40650b90741127
Thangaraj <thangarajatk@gmail.com>
2022-07-21 17:43:24 +0530

    Code refactoring and improvements

--- src/main/java/org/example/utils/configloader/PropertyUtils.java
+++ src/main/java/org/example/utils/configloader/PropertyUtils.java
@@ -15,8 +15,8 @@ public final class PropertyUtils {
 
     private static final Properties property = new Properties();
 
-    static void loadProperties(String propertyFilePath) {
-        try (FileInputStream input = new FileInputStream(propertyFilePath)) {
+    static void loadProperties() {
+        try (FileInputStream input = new FileInputStream(FrameworkConstants.CONFIG_PROPERTIES_PATH)) {
             property.load(input);
         } catch (IOException e) {
             throw new PropertyFileUsageException("IOException occurred while loading Property file in the specified path");
@@ -24,7 +24,7 @@ public final class PropertyUtils {
     }
 
     public static String getPropertyValue(ConfigProperties key) {
-        loadProperties(FrameworkConstants.CONFIG_PROPERTIES_PATH);
+        loadProperties();
         if (Objects.isNull(property.getProperty(key.name().toLowerCase()))) {
             throw new PropertyFileUsageException("Property name - " + key + " is not found. Please check the config.properties");
         }
